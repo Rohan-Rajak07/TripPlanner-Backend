e6c01395f8a330bd8d4f1cf39de23871efa132cb
Rohan-Rajak07 <rohanrajak132@gmail.com>
2026-09-10 10:51:15 +0530

    commit

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -5,7 +5,8 @@ import jwt from "jsonwebtoken";
 const cookieOptions = {
     httpOnly: true,
     secure: true,
-    sameSite: "none",
+    sameSite: "lax",
+    path: "/",
     maxAge: 7 * 24 * 60 * 60 * 1000
 };
 
@@ -74,9 +75,11 @@ export const logout=async(req,res)=>{
 
     try
     {
-        res.clearCookie("token",{
-            httpOnly:true,
-            secure:process.env.NODE_ENV==="production",
+        res.clearCookie("token", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "lax",
+        path: "/"
         });
         return res.json({success:true,message:"Logout successfully"});
     }catch(error)    
